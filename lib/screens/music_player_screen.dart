99749029a4a5479e5e6272555ace298c5c416485
HyunJun Baek <hjbaek5796@gmail.com>
2023-10-09 21:45:55 +0900

    [Music Player] rendered and implemented scrollable images using pageview and page_controller

--- lib/screens/music_player_screen.dart
+++ lib/screens/music_player_screen.dart
@@ -8,8 +8,53 @@ class MusicPlayerScreen extends StatefulWidget {
 }
 
 class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
+  final PageController _pageController = PageController(
+    viewportFraction: 0.8,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView.builder(
+            controller: _pageController,
+            itemCount: 5,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 350,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          "assets/covers/${index + 1}.jpeg",
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Text(
+                    'My Hero Academia',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Horikoshi Kouhei',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
